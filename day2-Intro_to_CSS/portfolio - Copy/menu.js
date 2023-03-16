344502220f7138f4fecfeb615fbdbfa92a48e3f7
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-03-16 23:25:10 +0530

    Reflexive UI

---
+++ day2-Intro_to_CSS/portfolio - Copy/menu.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("menu-button").addEventListener("click",showMenu)
+    document.getElementById("close-menu").addEventListener("click",closeMenu)
+    function showMenu() {
+        const menu = document.querySelector(".menu");
+        menu.style.height="100%";
+    }
+    function closeMenu() {
+        const menu = document.querySelector(".menu");
+        menu.style.height="0";
+    }
+})
