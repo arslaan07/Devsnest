ce349752b118d307bdf46d4d8f09c2b32ccde324
Arslaan Shakeel <108570502+arslaan07@users.noreply.github.com>
2023-03-17 23:56:09 +0530

    day7 cont

--- day2-Intro_to_CSS/portfolio - Copy/menu.js
+++ day2-Intro_to_CSS/portfolio - Copy/menu.js
@@ -1,12 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("menu-button").addEventListener("click",showMenu)
     document.getElementById("close-menu").addEventListener("click",closeMenu)
-    function showMenu() {
+        function showMenu() {
         const menu = document.querySelector(".menu");
         menu.style.height="100%";
+        console.log("showMenu");
     }
     function closeMenu() {
         const menu = document.querySelector(".menu");
         menu.style.height="0";
+        console.log("closeMenu");
+
     }
+    function addClickToMenuItems() {
+       let menuItems = document.querySelectorAll("section.menu ul > li");
+       for(let menuItem of menuItems) {
+        menuItem.addEventListener("click",closeMenu);
+       }
+    }
+    addClickToMenuItems();
 })
